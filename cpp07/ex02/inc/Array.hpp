7f5db02be5fe82b00eff2e2c8a5c58c30fb729ee
Arty <forgeard.arthur@hotmail.com>
2024-05-30 15:07:28 +0200

    Go 42

--- cpp07/ex02/inc/Array.hpp
+++ cpp07/ex02/inc/Array.hpp
@@ -2,25 +2,47 @@
 # define ARRAY_HPP
 
 # include <iostream>
+# include<exception>
 
 template<typename T>
 class Array
 {
     private:
         T *_array;
+        unsigned int _size;
     public:
-    Array    &Array::operator=(const Array &src){
-        *this = src;
-        return (*this);
-    };
-    Array(/* args */) : _array(NULL){};
-    Array(unsigned int n){
-        this->_array = new T[n];
-    };
-    Array(const Array &src){
-        *this = src;
-    };
-    ~Array();
+
+        class OutOfRange : public std::exception { 
+			public: 
+				virtual const char* what() const throw() {
+                    return ("Out of range.\n");
+                }; 
+		};
+
+        Array    &Array::operator=(const Array &src){
+            *this = src;
+            return (*this);
+        };
+
+        T	&Array<T>::operator[](unsigned int index) const {
+        	if (pos >= this->_size) {
+        		throw OutOfRange();
+        	}
+        	return (this->_array[index]);
+        }
+
+        Array(/* args */) : _array(NULL), _size(0){};
+
+        Array(unsigned int n){
+            this->_array = new T[n];
+            this->_size = n;
+        };
+
+        Array(const Array &src){
+            *this = src;
+        };
+
+        ~Array();
 };
 
 
