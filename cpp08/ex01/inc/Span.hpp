08da0c5afe38466433fcb7974b36d55c0ce70fed
Arty <forgeard.arthur@hotmail.com>
2024-06-03 13:46:02 +0200

    go 42

---
+++ cpp08/ex01/inc/Span.hpp
@@ -0,0 +1,28 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+class Span
+{
+private:
+    unsigned int _N;
+public:
+    void addNumber(int Number);
+    unsigned int shortestSpan(b);
+    unsigned int longestSpan();
+    Span();
+    Span(unsigned int N);
+    ~Span();
+};
+
+Span::Span(unsigned int N){
+}
+
+Span::Span(){
+}
+
+Span::~Span()
+{
+}
+
+
+#endif
